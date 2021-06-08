8fa350ed78754589da228150b099ba6132893dac
Tom <thomas.spencer@codurance.com>
2021-06-08 14:05:16 +0100

    Add hello route

---
+++ src/main/java/com/codurance/hellospringboot/WebController.java
@@ -0,0 +1,14 @@
+package com.codurance.hellospringboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+  @RequestMapping("/hello")
+  public String sayHello(){
+    System.out.println("saying hello world Spring Boot...");
+    return "hello";
+  }
+}
