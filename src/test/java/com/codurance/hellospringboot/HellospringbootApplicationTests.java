e6bf8783e16d8c2cfa2e0576e6d5f4e7f01a83c0
Tom <thomas.spencer@codurance.com>
2021-06-08 14:18:13 +0100

    Add test for controller

--- src/test/java/com/codurance/hellospringboot/HellospringbootApplicationTests.java
+++ src/test/java/com/codurance/hellospringboot/HellospringbootApplicationTests.java
@@ -1,13 +1,20 @@
 package com.codurance.hellospringboot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HellospringbootApplicationTests {
 
+  @Autowired
+  private WebController controller;
+
   @Test
   void contextLoads() {
+    assertThat(controller).isNotNull();
   }
 
 }
