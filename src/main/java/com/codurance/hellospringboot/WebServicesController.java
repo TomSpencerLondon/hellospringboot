e875a0d3e5101f4eac2a7acd2cd1512283a31d26
Tom <thomas.spencer@codurance.com>
2021-06-08 14:49:33 +0100

    Add rest controller

---
+++ src/main/java/com/codurance/hellospringboot/WebServicesController.java
@@ -0,0 +1,13 @@
+package com.codurance.hellospringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebServicesController {
+
+  @GetMapping("/rest")
+  public String sayREST() {
+    return "Great let's take a REST";
+  }
+}
